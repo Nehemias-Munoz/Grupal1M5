0fa8f5f6d88ebc3003ccfe1fa6a86cb4f8252ab9
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2023-06-16 11:47:09 -0400

    Crea loginServlet

---
+++ src/main/java/control/LoginServlet.java
@@ -0,0 +1,66 @@
+package control;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private final String USUARIO="";
+    private final String CLAVE="";
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		PrintWriter out = response.getWriter();
+		
+		String usuario = request.getParameter("usuario");
+		String clave = request.getParameter("clave");
+		 
+		 
+		 if(!(USUARIO.equals(usuario) && CLAVE.equals(clave))) {
+			 out.println("<script type\"text/javascript\">");
+			 out.println("alert('Usuario o password incorrecto')");
+			 out.println("location='index.jsp';");
+			 out.println("</scritp>");
+			 
+		 }else {
+			 HttpSession sesionUsuario = request.getSession(true);
+			 sesionUsuario.setAttribute("Nombre", sesionUsuario);
+			 RequestDispatcher rd = request.getRequestDispatcher("session.jsp");
+			 rd.forward(request, response);
+		 }
+	}
+
+}
