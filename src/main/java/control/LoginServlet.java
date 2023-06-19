601181bd4e739f49682a167e254e0e9ad08fae11
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2023-06-19 17:45:20 -0400

    login servlet

--- src/main/java/control/LoginServlet.java
+++ src/main/java/control/LoginServlet.java
@@ -1,5 +1,8 @@
 package control;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,18 +11,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private final String USUARIO="abcd";
     private final String CLAVE="1234";
-	
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -42,19 +42,19 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
+
 		PrintWriter out = response.getWriter();
-		
+
 		String usuario = request.getParameter("usuario");
 		String clave = request.getParameter("clave");
-		 
-		 
+
+
 		 if(!(USUARIO.equals(usuario) && CLAVE.equals(clave))) {
 			 out.println("<script type\"text/javascript\">");
 			 out.println("alert('Usuario o password incorrecto')");
 			 out.println("location='index.jsp';");
 			 out.println("</scritp>");
-			 
+
 		 }else {
 			 HttpSession sesionUsuario = request.getSession(true);
 			 sesionUsuario.setAttribute("Nombre", sesionUsuario);
